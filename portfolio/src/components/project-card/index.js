49681ee8c279a45fe5904cc946e37ab60029bd40
Ainhoa Prada <apt.code14@gmail.com>
2023-03-07 00:39:27 +0100

    card projects styling

--- portfolio/src/components/project-card/index.js
+++ portfolio/src/components/project-card/index.js
@@ -4,13 +4,18 @@ function ProjectCard (props) {
     return(
         <div className="project-card">
             <h3>{props.title}</h3>
-            <img src={props.img} alt={props.title} />
+            <img src={props.image} alt={props.title} />
             <div className="project-buttons">
-                <button className="p pages">Go to page</button>
-                <button className="p repo">Go to repository</button>
+                <button className="p pages">Page</button>
+                <button className="p repo">Repository</button>
             </div>
         </div>
         
     )
 }
 export default ProjectCard;
+
+//make the bg change in images
+//make images to show up!
+//add icons where pages and github repo
+//move navbar a bit more to the right
--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -1,19 +1,31 @@
-import "./index.css";
+import "./style.css";
 import ProjectCard from "../project-card";
-import projectList from './projects.json';
+import projectList from "./projects.json";
 
-function Projects () {
-    return(
-        <div>
+function Projects() {
+  return (
+    <div className="projects">
+      <div className="about-projects">
+        <p>
+          In this section you will find some projects where I've work in
+          collaboration and by myself. Some of them are part of a bootcamp but
+          anyways you will never read this
+        </p>
+      </div>
+      <div className="wrapper">
         {projectList.map(function project(p) {
-            return <ProjectCard
-                     key={p.id}
-                     title={p.title}
-                     img={p.img}
-                     page={p.page}
-                     repository={p.repository} />
+          return (
+            <ProjectCard
+              key={p.id}
+              title={p.title}
+              image={p.image}
+              page={p.page}
+              repository={p.repository}
+            />
+          );
         })}
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Projects;
