6ec6d77334316910a4ad95ff89790ed826b6510a
Ainhoa Prada <apt.code14@gmail.com>
2023-03-07 19:33:09 +0100

    Router bug fixed with BrowserRouter

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import Header from './components/header';
-import Home from './components/Home';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {faGlobe} from '@fortawesome/free-solid-svg-icons';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-library.add(faGithub, faLinkedin, faGlobe)
+import "./App.css";
+import Header from "./components/header";
+import Home from "./components/Home";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+library.add(faGithub, faLinkedin, faGlobe);
 
 function App() {
   return (
-    <Router>
-    <div className="App">
+      <BrowserRouter>
       <Header />
-      {/* On this app we are using react router dom */}
+        {/* On this app we are using react router dom */}
         <Routes>
-          <Route exact path="/" element={ <Home />}></Route>
-          <Route exact path='projects' element={<Projects />} />
-          <Route exact path='contact' element={<Contact />} />
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="projects" element={<Projects />} />
+          <Route exact path="contact" element={<Contact />} />
         </Routes>
-    </div>
-    </Router>
+      </BrowserRouter>
   );
 }
 
